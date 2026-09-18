9cd4ac598e5e25337781f59fa243daf0ba8dd327
ranveer <ranveersingh.pawar6627@gmail.com>
2026-09-18 16:15:12 +0530

    Initial commit from lab

---
+++ new.py
@@ -0,0 +1,21 @@
+#Celsius to Far.
+cel = int(input("Enter a value: "))
+far = (cel * 9/5) + 32
+print(far)
+
+#Kilometers to Meters
+kms = int(input("enter a value: "))
+ms = kms * 1000
+print(ms)
+
+#Interest calc.
+p = float(input("Enter Principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time period in years: "))
+
+interest = (p*r*t) / 100
+print(interest)
+
+#Percentage
+matho = int(input("enter math marks obtained"))
+matht = int(input("enter math marks obtained"))
